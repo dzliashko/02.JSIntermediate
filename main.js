82d9f1327f96e9e3dd2e702a84e32f4b21ed3d31
Dzmitry Liashko <dzliashko@gmail.com>
2026-07-22 15:49:01 +0300

    done

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 function makeCounter() {
     // Use a let binding inside this function and return a function
     // that increments and returns it.
+    let a = 0
+    function f() {
+        a++
+        return a
+    }
+    return f
 }
 
 const n = Number(require('fs').readFileSync(0, 'utf-8').trim());
